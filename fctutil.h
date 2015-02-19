abfa2f92d368818ac2ec527dc8eb57f0c85ec31f
whitefecaf <whitefecaf@localhost.localdomain>
2015-02-19 11:56:45 +0100

    ajout de 2 fichier oublier dans le dernier commit

---
+++ fctutil.h
@@ -0,0 +1,15 @@
+#ifndef __FCTUTIL_H__
+#define __FCTUTIL_H__
+
+#include "csapp.h"
+
+//refuse tous les signaux (utilités : lors d'une interuption on veut eviter qu'une autre interuption intervienne)
+void refusedSignal();
+
+//réautaurise les signaux (a la fin de l'interruption)
+void autorisedSignal();
+
+
+
+
+#endif
