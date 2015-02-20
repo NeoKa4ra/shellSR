69d24fff8e4f7a3ec791ecf370546b60a49da333
whitefecaf <whitefecaf@localhost.localdomain>
2015-02-20 11:15:49 +0100

    integration total de wait(verif non faite) avec sont affichage

--- fctutil.c
+++ fctutil.c
@@ -11,3 +11,20 @@ void autorisedSignal(){
 	sigemptyset(&p_ens);
 	sigprocmask(SIG_SETMASK, &p_ens, NULL);
 }
+
+
+
+int allStopped(jobsT *jb)
+{
+  int i;
+  int result = 1;
+  for(i = 0; i < taille; i++)
+    {
+      if (jb->jobs[i].etat != ST)
+	{
+	  result = 0;
+	  break;
+	}
+    }
+  return result;
+}
